86579dcdfd5c79df529e1c404db2db74fd7769c3
Gonzalo Cabezas Núñez <gonzalocn92@gmail.com>
2024-06-26 10:33:34 +0200

    Update titulos.js

--- js/titulos.js
+++ js/titulos.js
@@ -1,12 +1,12 @@
 let botonAvanzar = document.getElementById("avanzar");
 let botonRetroceder = document.getElementById("retroceder");
 let fotos = [
-    "../media/img/certificat_FRONT-END.png",
-    "../media/img/certificat_WEB-DEVELOPMENT.png",
-    "../media/img/certificat_HTML.png",
-    "../media/img/certificat_CSS.png",
-    "../media/img/certificat_JS.png",
-    "../media/img/certificat_JS-2.png",
+    "media/img/certificat_FRONT-END.png",
+    "media/img/certificat_WEB-DEVELOPMENT.png",
+    "media/img/certificat_HTML.png",
+    "media/img/certificat_CSS.png",
+    "media/img/certificat_JS.png",
+    "media/img/certificat_JS-2.png",
 ];
 let div = document.getElementById("divParaImg");
 let img = document.createElement("img");
