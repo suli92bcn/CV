e932d4f06b2efdbef9a0ba4adc9c2fa9e8b5165c
Gonzalo Cabezas Núñez <gonzalocn92@gmail.com>
2024-06-27 18:49:04 +0200

    Update titulos.js

--- js/titulos.js
+++ js/titulos.js
@@ -11,37 +11,53 @@ let fotos = [
 let div = document.getElementById("divParaImg");
 let img = document.createElement("img");
 let posicionActual = 0;
+let intervaloRefresco;
+
+function iniciarIntervalo() {
+    intervaloRefresco = setInterval(avanzarFoto, 6000);
+}
+
+function reiniciarIntervalo() {
+    clearInterval(intervaloRefresco);
+    iniciarIntervalo();
+}
+
 function avanzarFoto() {
     if (posicionActual >= (fotos.length - 1)) {
         posicionActual = 0;
-    }
-    else {
+    } else {
         posicionActual = posicionActual + 1;
     }
     renderizarImagen();
 }
+
 function retrocederFoto() {
     if (posicionActual <= 0) {
         posicionActual = fotos.length - 1;
-    }
-    else {
+    } else {
         posicionActual--;
     }
     renderizarImagen();
 }
+
 function renderizarImagen() {
     img.src = fotos[posicionActual];
     div.innerHTML = '';
     div.appendChild(img);
     console.log('imagen mostrada', img.src);
 }
+
 renderizarImagen();
 botonAvanzar.addEventListener("click", function(event) {
     event.preventDefault();
     avanzarFoto();
+    reiniciarIntervalo();
 });
+
 botonRetroceder.addEventListener("click", function(event) {
     event.preventDefault();
     retrocederFoto();
+    reiniciarIntervalo();
 });
-setInterval(avanzarFoto, 6000);
+
+iniciarIntervalo();
