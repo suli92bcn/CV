44945f1b85e881551fb5140eefbf519a7466826a
Gonzalo Cabezas Núñez <gonzalocn92@gmail.com>
2024-10-14 16:59:52 +0200

    Delete js directory

--- js/cv.js
+++
@@ -1,8 +0,0 @@
-import * as cookies from './cookies.js';
-cookies.initCookieBar();
-
-import { iniciarCarrousel } from './carrousel.js';
-iniciarCarrousel();
-
-import { abrirModal } from './modal.js';
-abrirModal('.pdfLink', '#pdfModal', '#pdfViewer', '.close');
